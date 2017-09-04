abb0b8f1f7d57c5639e0bc86e962f6a667ae046f
Will Speak <lithiumflame@gmail.com>
2017-09-04 16:00:55 +0100

    Create Install Script
    Initial work on making top-level install script.

--- brew/install.sh
+++ brew/install.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+which brew > /dev/null 2>&1
+if ! $?
+then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Tap the brew bundle command
 brew tap homebrew/bundle
 cd $(dirname $0)/.
---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s ${DIR}/.profile ~/.profile
+ln -s ${DIR}/.emacs.d ~/.emacs.d
+ln -s ${DIR}/.gitconfig ~/.gitconfig
+ln -s ${DIR}/.gitignore_global ~/.gitignore_global
+ln -s ${DIR}/.inputrc ~/.inputrc
+
+brew/install.sh
