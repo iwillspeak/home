20d49cdba12b53b7410875fa212a692c3cf5058f
Will Speak <lithiumflame@gmail.com>
2017-09-11 09:38:18 +0100

    Add OpenSSL for .NET 1.1

--- brew/install.sh
+++ brew/install.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
 if ! which brew > /dev/null 2>&1
 then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -7,5 +10,7 @@ fi
 
 # Tap the brew bundle command
 brew tap homebrew/bundle
-cd $(dirname $0)/.
 brew bundle
+
+ln -sh /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+ln -sh /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
