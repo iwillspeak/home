5dc7bde81527302070b84a7a563c80cb34e54390
Will Speak <lithiumflame@gmail.com>
2018-03-09 19:51:23 +0000

    Fixup Color Theme Installation

--- colourtheme/install.sh
+++ colourtheme/install.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+
+
 curl -XGET -vv https://codeload.github.com/dracula/terminal.app/zip/master -o $TMPDIR/Dracula.zip
 unzip -o $TMPDIR/Dracula.zip -d $TMPDIR/Dracula/
 open -a Terminal $TMPDIR/Dracula/**/*.terminal
-osascript -e <<EOF
-tell application Terminal
-	 set default settings to settings set Dracula
+osascript -e '
+tell application "Terminal"
+	 set default settings to settings set "Dracula"
 end tell
-EOF
+'
