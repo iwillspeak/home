7728fab9de817cf8e991365cdc0ca50708a470e3
Will Speak <lithiumflame@gmail.com>
2019-02-14 20:11:44 +0000

    Add VS Font Fixup to Install Script
    Stop this happening in future. Hopefully this is a little more
    targetted than the hack I initially made on my work Mac.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# link up the dotfiles containing global settings
 ln -sh ${DIR}/.profile ~/.profile
 ln -sh ${DIR}/.emacs.d ~/.emacs.d
 ln -sh ${DIR}/.gitconfig ~/.gitconfig
@@ -10,6 +11,11 @@ ln -sh ${DIR}/.inputrc ~/.inputrc
 ln -sh ${DIR}/.bashrc ~/.bashrc
 ln -sh ${DIR}/.bash_profile ~/.bash_profile
 
+# install the Terminal colour theme
 colourtheme/install.sh
 
+# Install Homebrew and packages
 brew/install.sh
+
+# Fixup the font rendering in Visual Studio Code
+defaults write com.microsoft.VSCode.helper CGFontRenderingFontSmoothingDisabled -bool NO
